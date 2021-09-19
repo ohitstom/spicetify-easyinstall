68500a84f0d184ef7cb746d93d01538a136f8d1d
Tom <22730962+OhItsTom@users.noreply.github.com>
2021-09-19 15:13:41 +0100

    Updated Links

--- modules/globals.py
+++ modules/globals.py
@@ -1,3 +1,3 @@
-FULL_SETUP_URL = 'https://github.com/OhItsTom/Spotify-Version/releases/download/spotify-1-1-62-583/spotify-1-1-62-583.exe'
-OLD_EXTRACTED_URL = 'https://github.com/OhItsTom/Spotify-Version/releases/download/yourmum/Extracted.zip'
+FULL_SETUP_URL = 'https://github.com/OhItsTom/EasyInstall-Dependencies/releases/download/spotify-1-1-62-583/spotify-1-1-62-583.exe'
+OLD_EXTRACTED_URL = 'https://github.com/OhItsTom/EasyInstall-Dependencies/releases/download/yourmum/Extracted.zip'
 DOWNLOAD_THEME_URL = "https://github.com/morpheusthewhite/spicetify-themes/archive/cbd332f7c683575f65b102b662ee9fd008d4d284.zip"
