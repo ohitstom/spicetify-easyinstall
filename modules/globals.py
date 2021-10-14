4842ce6e876bacce869289044d47a8b4e8e4c5e6
Tom <22730962+ohitstom@users.noreply.github.com>
2021-10-14 23:36:05 +0100

    Update globals.py

--- modules/globals.py
+++ modules/globals.py
@@ -4,4 +4,3 @@ THEMES_EXTRACTED   = "\spicetify-themes-59f274fedbec29993afc51658c8b369fa4d25e26
 
 FULL_SETUP_URL     = "https://upgrade.scdn.co/upgrade/client/win32-x86/spotify_installer-1.1.69.612.gb7409abc-16.exe"
 DOWNLOAD_THEME_URL = "https://github.com/morpheusthewhite/spicetify-themes/archive/59f274fedbec29993afc51658c8b369fa4d25e26.zip"
-OLD_EXTRACTED_URL  = "https://github.com/OhItsTom/EasyInstall-Dependencies/releases/download/yourmum/Extracted.zip"
