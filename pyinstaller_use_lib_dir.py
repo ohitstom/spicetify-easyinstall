c31e7f89ad8570fb05a53c7a3fc21c2c0586f07f
Tom <22730962+ohitstom@users.noreply.github.com>
2021-11-28 20:20:06 +0000

    Delete pyinstaller_use_lib_dir.py

--- pyinstaller_use_lib_dir.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), "lib"))
-import ctypes
-
-ctypes.windll.kernel32.SetDllDirectoryW(
-    os.path.join(os.path.dirname(sys.argv[0]), "lib")
-)
